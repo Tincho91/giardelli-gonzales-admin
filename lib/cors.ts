7e64c7bafe64dee9091b1e0f6cfc7b05c0d81b90
Martin Maiaru <tincho_91_15@hotmail.com>
2023-11-05 18:25:52 -0300

    Demo 3 - 5/11 - 1

--- lib/cors.ts
+++ lib/cors.ts
@@ -1,6 +1,6 @@
 const setCorsHeaders = (res: Response) => {
   // Replace the following with the origin(s) you want to allow
-  const ALLOWED_ORIGINS = ['http://localhost:3001'];
+  const ALLOWED_ORIGINS = ['https://giardelli-gonzalez-web.vercel.app'];
 
   const origin = res.headers.get('Origin');
 
@@ -8,7 +8,7 @@ const setCorsHeaders = (res: Response) => {
     res.headers.set('Access-Control-Allow-Origin', origin || '*');
   }
   
-  res.headers.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  res.headers.set('Access-Control-Allow-Methods', 'GET, DELETE, PATCH, POST, PUT, OPTIONS');
   res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.headers.set('Access-Control-Allow-Credentials', 'true');
   
